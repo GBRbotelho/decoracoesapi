6d505094f9c88d1d64f6c4cec115557819d083e8
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-05-07 20:50:29 -0300

    Att Console

--- src/application/useCases/client/CreateClient.js
+++ src/application/useCases/client/CreateClient.js
@@ -19,6 +19,8 @@ module.exports = async (clientData, clientRepository) => {
       clientData.complemento
     );
 
+    console.log("Criando Cliente no DB");
+
     return clientRepository.create(client);
   } catch (error) {
     throw { message: error.message || "Failed to Create client" };
