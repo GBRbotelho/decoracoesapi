3d76e4c4f4ebc78062a0987a25693b0b674ef070
GBRbotelho <gabrielbbotelho05@gmail.com>
2024-09-10 15:50:45 -0300

    Att Subscription Token Data

--- src/application/useCases/user/DataToken.js
+++ src/application/useCases/user/DataToken.js
@@ -1,5 +1,7 @@
 const TokenService = require("../../security/TokenService");
 const repository = require("../../../infrastructure/repositories/userRepository");
+const SubscriptionRepository = require("../../../infrastructure/repositories/subscriptionRepository");
+const subscriptionRepository = require("../../../infrastructure/repositories/subscriptionRepository");
 
 const DataToken = async (token) => {
   try {
@@ -13,6 +15,13 @@ const DataToken = async (token) => {
     }
     const tokenDecoded = TokenService.decode(token.replace("Bearer ", ""));
     let user = await repository.find({ id: tokenDecoded.id });
+
+    const subscriptions = await subscriptionRepository.findAll({
+      userId: user.id,
+    });
+    if (subscriptions) {
+      user.subscriptions = subscriptions;
+    }
     user.password = "";
     return {
       success: true,
